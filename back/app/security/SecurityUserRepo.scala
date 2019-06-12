36ee5a84ec6a20923f0cfb46ddcf2b6899b62b5e
Aimad <goldensunlostage@gmail.com>
2019-06-12 17:42:22 +0200

    WIP Silhouette

---
+++ back/app/security/SecurityUserRepo.scala
@@ -0,0 +1,44 @@
+package security
+
+import javax.inject.Inject
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import slick.jdbc.JdbcProfile
+import slick.lifted.ProvenShape
+
+import scala.concurrent.{ExecutionContext, Future}
+
+case class SecurityUser(id : Option[Long], userId : Long, securityId : String, origin : String)
+
+class SecurityUserRepo @Inject()
+(protected val userRepo: UserRepo)
+(implicit ec : ExecutionContext, protected val dbConfigProvider: DatabaseConfigProvider)
+  extends HasDatabaseConfigProvider[JdbcProfile] {
+  import profile.api._
+
+  class SecurityUsers(tag : Tag) extends Table[SecurityUser](tag, "security_users") {
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+    def securityId = column[String]("securityId")
+    def origin = column[String]("origin")
+
+    def userId = column[Long]("userId")
+    def userFk =
+      foreignKey("USER_FK", userId, userRepo.users)(_.id, ForeignKeyAction.Restrict, ForeignKeyAction.Cascade)
+
+    override def * : ProvenShape[SecurityUser] =
+      (id.?, userId, securityId, origin) <> (SecurityUser.tupled, SecurityUser.unapply)
+
+  }
+
+  val securityUsers = TableQuery[SecurityUsers]
+  val setup = DBIO.seq(securityUsers.schema.create)
+  val setupFuture: Future[Unit] = db.run(setup)
+
+  def byId(id : Long) : Future[Option[SecurityUser]] =
+    db.run(securityUsers.filter(_.id === id).result).map(_.headOption)
+
+  def add(securityUser: SecurityUser) : Future[Int] = db.run(securityUsers += securityUser)
+
+  def bySecurityId(id : String) : Future[Option[SecurityUser]] =
+    db.run(securityUsers.filter(_.securityId === id).result).map(_.headOption)
+
+}
--- back/build.sbt
+++ back/build.sbt
@@ -5,8 +5,8 @@ version := "1.0"
 lazy val `back` = (project in file(".")).enablePlugins(PlayScala)
 
 resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
-      
 resolvers += "Akka Snapshot Repository" at "http://repo.akka.io/snapshots/"
+resolvers += "Atlassian Releases" at "https://maven.atlassian.com/public/"
       
 scalaVersion := "2.12.2"
 
@@ -16,8 +16,15 @@ libraryDependencies ++= Seq(
   "com.typesafe.play" %% "play-slick" % "3.0.0",
   "com.typesafe.play" %% "play-json" % "2.6.0",
   "com.h2database" % "h2" % "1.4.199",
+  guice,
+  /*
+  "com.mohiva" %% "play-silhouette" % "5.0.2",
+  "com.mohiva" %% "play-silhouette-persistence" % "5.0.2",
+  "com.mohiva" %% "play-silhouette-password-bcrypt" % "5.0.2",
+  "com.mohiva" %% "play-silhouette-crypto-jca" % "5.0.2",
+  */
+
   "org.scalatestplus.play" %% "scalatestplus-play" % "3.0.0" % "test",
-  specs2 % Test,
-  guice
+  specs2 % Test
 
 )
