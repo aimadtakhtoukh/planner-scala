4fd8bb4be5f8b899dbf14c3cccff15319581f4f8
Aimad <goldensunlostage@gmail.com>
2019-08-20 17:13:39 +0200

    WIP Front routing

---
+++ front/src/components/TokenHandling.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Token!
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TokenHandling",
+        mounted() {
+            console.info(this.$route.query.code)
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- front/src/routes.js
+++ front/src/routes.js
@@ -1,16 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import TokenHandling from './components/TokenHandling'
 
 Vue.use(VueRouter);
 
 const Default = {template : "<div>Pas là frère</div>"};
-const TokenHandling = {template : "<div>Token!</div>"};
 
 const router = new VueRouter({
     routes : [
         {path : '/token/discord', component: TokenHandling},
         {path : '/pasla', component: Default},
-    ]
+    ],
+    mode : 'history'
 });
 
 export default router
