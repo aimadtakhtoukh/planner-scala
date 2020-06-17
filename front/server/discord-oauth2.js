c93e8e2aea34fa9925e837ac520e07e6ef05c310
Aimad <aimad.takhtoukh@gmail.com>
2020-06-17 02:51:08 +0200

    Node Fetch est mal utilisé, je corrige

--- front/server/discord-oauth2.js
+++ front/server/discord-oauth2.js
@@ -8,26 +8,32 @@ const router = express.Router();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const redirect = encodeURIComponent(process.env.REDIRECT_URL + '/api/discord/callback');
+const redirect = process.env.REDIRECT_URL + '/api/discord/callback';
+const redirectEncoded = encodeURIComponent(redirect);
 const scope = "identify";
 
 router.get('/login', (req, res) => {
-    res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${CLIENT_ID}&scope=${scope}&response_type=code&redirect_uri=${redirect}`);
+    res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${CLIENT_ID}&scope=${scope}&response_type=code&redirect_uri=${redirectEncoded}`);
 });
 
 router.get('/callback', catchAsync(async (req, res) => {
-    if (!req.query.code) throw new Error('NoCodeProvided');
-    const code = req.query.code;
-    const creds = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
-    const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect}`,
-        {
-            method: 'POST',
-            headers: {
-                Authorization: `Basic ${creds}`,
-            },
-        });
-    const json = await response.json();
-    res.redirect(`/token/discord/?access_token=${json.access_token}&refresh_token=${json.refresh_token}&expires_in=${json.expires_in}`);
+  if (!req.query.code) throw new Error('NoCodeProvided');
+  const code = req.query.code;
+  let params = new URLSearchParams()
+  params.append('client_id', CLIENT_ID);
+  params.append('client_secret', CLIENT_SECRET);
+  params.append('grant_type', 'authorization_code');
+  params.append('code', code);
+  params.append('redirect_uri', redirect);
+  params.append('scope', scope);
+  const response = await fetch(`https://discordapp.com/api/oauth2/token`,
+    {
+        method: 'POST',
+        body : params
+    }
+  );
+  const json = await response.json();
+  res.redirect(`/token/discord/?access_token=${json.access_token}&refresh_token=${json.refresh_token}&expires_in=${json.expires_in}`);
 }));
 
 router.get('/valid', catchAsync(async (req, res) => {
@@ -53,14 +59,18 @@ router.get('/valid', catchAsync(async (req, res) => {
 router.get('/refresh', catchAsync(async (req, res) => {
   if (!req.query.refresh_token) throw new Error('NoCodeProvided');
   const refresh_token = req.query.refresh_token;
-  const creds = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
-  const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=refresh_token&refresh_token=${refresh_token}&redirect_uri=${redirect}`,
+  let params = new URLSearchParams()
+  params.append('client_id', CLIENT_ID);
+  params.append('client_secret', CLIENT_SECRET);
+  params.append('grant_type', 'refresh_token');
+  params.append('refresh_token', refresh_token);
+  params.append('redirect_uri', redirect);
+  const response = await fetch(`https://discordapp.com/api/oauth2/token`,
     {
       method: 'POST',
-      headers: {
-        Authorization: `Basic ${creds}`,
-      },
-    });
+      body : params
+    }
+  );
   const json = await response.json();
   res.status(200).send({
     access_token : json.access_token,
